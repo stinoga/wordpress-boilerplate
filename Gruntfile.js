a349b612095b4472389f353e507c7ef56fefc99c
Rob Stinogle <stinogle@gmail.com>
2013-09-28 15:18:32 -0400

    Aut prefixer setup.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,127 +1,143 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
+  grunt.initConfig({
 
-		// Watches for changes and runs tasks
-		watch : {
-			sass : {
-				files : ['scss/**/*.scss'],
-				tasks : ['sass:dev'],
-				options : {
-					livereload : true
-				}
-			},
-			js : {
-				files : ['js/**/*.js'],
-				tasks : ['jshint'],
-				options : {
-					livereload : true
-				}
-			},
-			php : {
-				files : ['**/*.php'],
-				options : {
-					livereload : true
-				}
-			}
-		},
+    // Watches for changes and runs tasks
+    watch : {
+      sass : {
+        files : ['scss/**/*.scss'],
+        tasks : ['sass:dev', 'autoprefixer'],
+        options : {
+          livereload : true
+        }
+      },
+      js : {
+        files : ['js/**/*.js'],
+        tasks : ['jshint'],
+        options : {
+          livereload : true
+        }
+      },
+      php : {
+        files : ['**/*.php'],
+        options : {
+          livereload : true
+        }
+      }
+    },
 
-		// JsHint your javascript
-		jshint : {
-			all : ['js/*.js', '!js/modernizr.js', '!js/*.min.js', '!js/vendor/**/*.js'],
-			options : {
-				browser: true,
-				curly: false,
-				eqeqeq: false,
-				eqnull: true,
-				expr: true,
-				immed: true,
-				newcap: true,
-				noarg: true,
-				smarttabs: true,
-				sub: true,
-				undef: false
-			}
-		},
+    // JsHint your javascript
+    jshint : {
+      all : ['js/*.js', '!js/modernizr.js', '!js/*.min.js', '!js/vendor/**/*.js'],
+      options : {
+        browser: true,
+        curly: false,
+        eqeqeq: false,
+        eqnull: true,
+        expr: true,
+        immed: true,
+        newcap: true,
+        noarg: true,
+        smarttabs: true,
+        sub: true,
+        undef: false
+      }
+    },
 
-		// Dev and production build for sass
-		sass : {
-			production : {
-				files : [
-					{
-						src : ['**/*.scss', '!**/_*.scss'],
-						cwd : 'scss',
-						dest : 'css',
-						ext : '.css',
-						expand : true
-					}
-				],
-				options : {
-					style : 'compressed',
+    // Dev and production build for sass
+    sass : {
+      production : {
+        files : [
+          {
+            src : ['**/*.scss', '!**/_*.scss'],
+            cwd : 'scss',
+            dest : 'css',
+            ext : '.css',
+            expand : true
+          }
+        ],
+        options : {
+          style : 'compressed',
           compass: true
-				}
-			},
-			dev : {
-				files : [
-					{
-						src : ['**/*.scss', '!**/_*.scss'],
-						cwd : 'scss',
-						dest : 'css',
-						ext : '.css',
-						expand : true
-					}
-				],
-				options : {
-					style : 'expanded',
+        }
+      },
+      dev : {
+        files : [
+          {
+            src : ['**/*.scss', '!**/_*.scss'],
+            cwd : 'scss',
+            dest : 'css',
+            ext : '.css',
+            expand : true
+          }
+        ],
+        options : {
+          style : 'expanded',
           compass: true
-				}
-			}
-		},
+        }
+      }
+    },
 
-		// Image min
-		imagemin : {
-			production : {
-				files : [
-					{
-						expand: true,
-						cwd: 'images',
-						src: '**/*.{png,jpg,jpeg}',
-						dest: 'images'
-					}
-				]
-			}
-		},
+    // Auto prefixer settings for browser prefixes
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions']
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: '**/*.css',
+          dest: 'css'
+        }]
+      }
+    },
 
-		// SVG min
-		svgmin: {
-			production : {
-				files: [
-					{
-						expand: true,
-						cwd: 'images',
-						src: '**/*.svg',
-						dest: 'images'
-					}
-				]
-			}
-		},
+    // Image min
+    imagemin : {
+      production : {
+        files : [
+          {
+            expand: true,
+            cwd: 'images',
+            src: '**/*.{png,jpg,jpeg}',
+            dest: 'images'
+          }
+        ]
+      }
+    },
 
-	});
+    // SVG min
+    svgmin: {
+      production : {
+        files: [
+          {
+            expand: true,
+            cwd: 'images',
+            src: '**/*.svg',
+            dest: 'images'
+          }
+        ]
+      }
+    },
 
-	// Default task
-	grunt.registerTask('default', ['watch']);
+  });
 
-	// Build task
-	grunt.registerTask('build', ['jshint', 'sass:production', 'imagemin:production', 'svgmin:production']);
+  // Default task
+  grunt.registerTask('default', ['watch']);
 
-	// Template Setup Task
-	grunt.registerTask('setup', ['sass:dev'])
+  // Build task
+  grunt.registerTask('build', ['jshint', 'sass:production', 'imagemin:production', 'svgmin:production']);
 
-	// Load up tasks
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
-	grunt.loadNpmTasks('grunt-svgmin');
+  // Template Setup Task
+  grunt.registerTask('setup', ['sass:dev'])
+
+  // Load up tasks
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-svgmin');
 
 };
